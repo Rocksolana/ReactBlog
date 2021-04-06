28c75e6affca0023c3cf7901ffc421067b06ad6a
ВАШЕ_ИМЯ <tsypuk@gmail.com>
2021-04-06 17:42:25 +0300

    creaded 2 folder Designer_item and DesignerImage

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import DesignerImage from './DesignerImage'
+import Designer_item from './Designer_item'
+import './main.css'
+
 
 
 const Main = () => {
     return (
-        <h1>Main</h1>
+     <section className="designers">
+        <div className="conteiner">
+            <div className="row header-row">
+                <div className="col-sm-12">
+                    <div className="wrapper">
+                        <div className="col-sm-4">
+                            <DesignerImage/>
+                        </div>
+                        <div className="col-sm-8">
+                            <Designer_item/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     )
 }
 

c48ebfd24f25305f19992eca3e9d6995fbe23552
ВАШЕ_ИМЯ <tsypuk@gmail.com>
2021-04-06 16:29:23 +0300

    added logo

--- src/App/Header/Logo/Logo.js
+++ src/App/Header/Logo/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from './logo.png'
  
 const Logo = () => {
     return (
         <div className="logo">
-        <img src="images/logo.png" alt="Logo"/>
+        <img src={logo} alt="Logo"/>
         </div>
     )
  }
